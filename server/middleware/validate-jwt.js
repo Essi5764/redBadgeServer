2305e067a30f4df1751a2d88d61af3afe45a23f1
Essi Monneus <essimonneus@gmail.com>
2021-07-03 21:33:52 -0400

    pushing to heroku

--- server/middleware/validate-jwt.js
+++ server/middleware/validate-jwt.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const {UserModel} = require("../models");
-const {EnrollModel} = require("../models");
+
 
 
 
@@ -34,41 +34,6 @@ const validateJWT = async (req, res, next) => {
         }
     
 };
-
-// const validateJWT = async (req, res, next) => {
-//     if (req.method == "OPTIONS"){
-//         next();
-//     }else if(
-//         req.headers.authorization 
-//     ){
-//         const {authorization} = req.headers;
-//         console.log("authorization -->", authorization);
-//         const payload = authorization
-//         ? jwt.verify(token, process.env.JWT_SECRET) : undefined
-//         console.log("payload -->", payload);
-//         if(payload){
-//             let foundUser = await UserModel.findOne({where: {id:payload.id}});
-//             console.log("foundUser -->", foundUser);
-//             if (foundUser){
-//                 console.log("request -->", req);
-//                 req.user = foundUser;
-//                 next();
-//             }else{
-//                 res.status(400).send({message: "Not Authorized"});
-//             }
-//         }else{
-//             res.status(401).send({message: "Invalid token"});
-//             }
-//         } else{
-//             res.status(403).send({message: "Forbidden"});
-//         }
-    
-// };
-
-
-
-
-
 
 
 module.exports = validateJWT;
