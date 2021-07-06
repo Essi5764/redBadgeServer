f63eee8bf738548964ce204b3ce3d456fa0e809a
Essi Monneus <essimonneus@gmail.com>
2021-07-06 06:16:39 -0400

    Validate-teacher was modified

--- server/middleware/validate-teacher.js
+++ server/middleware/validate-teacher.js
@@ -32,38 +32,3 @@ const validateTeacher = async (req, res, next) => {
 
 module.exports = validateTeacher;
 
-// const UserModel = require ("../models/user");
-// require ("dotenv").config();
-// const jwt = require("jsonwebtoken");
-// const chalk = require('chalk');
-
-// const validateTeacher = (req, res, next) => {
-//     const token = req.headers.authorization;
-//     if (req.method === "OPTIONS") {
-//       return next();
-//     } else if (!token) {
-//       return res.status(403).send({ auth: false, message: "No token provided" });
-//     } else {
-//       jwt.verify(token, process.env.JWT_SECRET, (err, decodeToken) => {
-//           console.log(chalk.blueBright("TOKEN: ", token))
-//           console.log(chalk.redBright("DECODED TOKEN: ", decodeToken))
-//         if (!err && decodeToken) {
-//           UserModel.findOne({
-//             where: {
-//               id: decodeToken.id,
-//             },
-//           })
-//             .then((user) => {
-//               if (!user) throw err;
-//               req.user = user;
-//               return next();
-//             })
-//             .catch((err) => next(err));
-//         } else {
-//           req.errors = err;
-//           return res.status(500).send("Not Authorized");
-//         }
-//       });
-//     }
-//   };
-//   module.exports = validateTeacher;
